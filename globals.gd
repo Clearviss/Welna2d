74543ff0dfad0e8442c309f32fdb4feb8bd04364
Dominik Rusinek <dominik.rusinek2021p@zsepoznan.pl>
2025-11-16 20:40:13 +0100

    UI & AI & Logic update

---
+++ globals.gd
@@ -0,0 +1,3 @@
+extends Node
+var score = 0
+var health = 5
--- poziom.gd
+++ poziom.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+
+
 var laser = preload("res://laser.tscn")
 
 # Called when the node enters the scene tree for the first time.
@@ -9,12 +11,29 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	$CanvasLayer.update_text()
+	$CanvasLayer.update_health()
 
 
 func _on_player_laser(bullet_pos, dyrekcja):
 	var current_lasereczek = laser.instantiate()
 	current_lasereczek.position = bullet_pos
 	current_lasereczek.rotation = dyrekcja.angle()
-	current_lasereczek.direction = dyrekcja
+	current_lasereczek.direction = dyrekcja + Vector2(randf()*0.3, randf()*0.3)
 	$Projectiles.add_child(current_lasereczek)
+
+
+func _on_button_2_pressed():
+	get_tree().change_scene_to_file("res://menu.tscn")
+	Globals.score = 0
+	Globals.health = 5
+
+
+func _on_kill_area_1_body_entered(body):
+	body.queue_free()
+func _on_kill_area_2_body_entered(body):
+	body.queue_free() 
+func _on_kill_area_3_body_entered(body):
+	body.queue_free() 
+func _on_kill_area_4_body_entered(body):
+	body.queue_free() 
